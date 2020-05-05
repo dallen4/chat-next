e8a5a66bc86620849a713ceab3302ab6e5fd2e71
Nieky Allen <nieky.allen@gmail.com>
2020-05-05 05:36:50 -0500

    update theme types and values

--- src/types/createPalette.d.ts
+++ src/types/createPalette.d.ts
@@ -20,11 +20,20 @@ declare module '@material-ui/core/styles/createPalette' {
         footer: ColorVariants;
     }
 
+    interface SystemColors {
+        error?: string;
+        warning?: string;
+        info?: string;
+        success?: string;
+    }
+
     export interface Palette extends BasePalette {
         custom: Partial<CustomColors>;
+        system: SystemColors;
     }
 
     export interface PaletteOptions extends BasePaletteOptions {
         custom: Partial<CustomColors>;
+        system: Partial<SystemColors>;
     }
 }
