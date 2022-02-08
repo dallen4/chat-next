a0ceac321509361bd857d90263817152b3d42f12
Nieky Allen <nieky.allen@gmail.com>
2022-02-08 00:20:58 -0600

    update to Html component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core';
 
 class CustomDocument extends Document {
     render() {
         return (
-            <html lang="en">
+            <Html lang="en">
                 <Head>
                     <meta charSet="utf-8" />
                     <meta
@@ -25,7 +25,7 @@ class CustomDocument extends Document {
                     <Main />
                     <NextScript />
                 </body>
-            </html>
+            </Html>
         );
     }
 }
