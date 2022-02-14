54623a80735cee67175adc1e1aee8d02d38715a1
Nieky Allen <nieky.allen@gmail.com>
2022-02-14 00:29:39 -0600

    cast isClient as bool

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
 
-export const isClient = typeof window !== 'undefined' && window.document && window.document.createElement;
+export const isClient = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
