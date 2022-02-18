59c6b58033f358d127bbf14f882de3b780f02683
Nieky Allen <nieky.allen@gmail.com>
2022-02-18 01:13:30 -0600

    start error handler

--- src/lib/peer/index.ts
+++ src/lib/peer/index.ts
@@ -1,3 +1,4 @@
+import { PeerErrorType, PeerErrorTypes } from 'lib/constants';
 import Peer from 'peerjs';
 
 export const initPeer = (id: string) => new Peer(id, {
@@ -27,4 +28,19 @@ export const getMediaStream = async (audioOnly = false): Promise<MediaStream> =>
     const stream = await navigator.mediaDevices.getUserMedia(constraintOptions);
 
     return stream;
+};
+
+const handleError = <T extends { type: PeerErrorType}>(err: T) => {
+    let message = 'Error occurred, connections may be unstable';
+
+    switch (err.type) {
+        case PeerErrorTypes.BrowserIncompatible:
+            message = 'Your browser does not support WebRTC';
+            break;
+        case PeerErrorTypes.Disconnected:
+            message = 'You have disconnnected from the network';
+            break;
+    }
+
+    return;
 };
