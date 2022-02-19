bd720422be88ac4a96f1f869b08976cf786bfe5d
Nieky Allen <nieky.allen@gmail.com>
2022-02-19 04:06:24 -0600

    impl random color functionality

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,7 +1,25 @@
 import { faker } from '@faker-js/faker';
+import randomColor from 'randomcolor';
 
 export const generateUsername = () => {
     const animal = faker.animal.type();
     const animalName = animal[0].toUpperCase() + animal.slice(1);
     return faker.commerce.productAdjective() + animalName;
+};
+
+export const generateAvatarColor = () => {
+    return randomColor({ luminosity: 'bright' });
+};
+
+const hues = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+
+export const generateColorSet = () => {
+    const hue1 = hues[Math.floor(Math.random() * hues.length)];
+    const hue2 = hues[Math.floor(Math.random() * hues.length)];
+    const hue3 = hues[Math.floor(Math.random() * hues.length)];
+
+    const color1 = randomColor({ hue: hue1, luminosity: 'bright' });
+    const color2 = randomColor({ hue: hue2, luminosity: 'bright' });
+    const color3 = randomColor({ hue: hue3, luminosity: 'bright' });
+    return [color1, color2, color3];
 };
