ab4ad4bf36c1c1c44bd4490bbab4d5a17654f8ab
Nieky Allen <nieky.allen@gmail.com>
2022-02-19 04:07:22 -0600

    flush out dynamic rendering for MessageItem

--- src/atoms/MessageItem.tsx
+++ src/atoms/MessageItem.tsx
@@ -1,16 +1,76 @@
+import { Avatar } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 import ListItem from '@material-ui/core/ListItem';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/lab/Alert';
+import clsx from 'clsx';
+import { useChat } from 'contexts/ChatContext';
+import { generateAvatarColor } from 'lib/util';
 import React from 'react';
 import { Message } from 'types/message';
 
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        item: {
+            '&:not(:first-child)': {
+                marginTop: theme.spacing(0.75),
+            },
+        },
+        white: {
+            color: 'white',
+        },
+        alert: {
+            width: '100%',
+            color: 'white',
+        },
+        avatar: {
+            color: 'white',
+            marginRight: theme.spacing(1.5),
+        },
+        author: {
+            fontWeight: 'bold',
+            paddingBottom: theme.spacing(0.5),
+        },
+    }),
+);
+
 const MessageItem = (props: MessageItemProps) => {
+    const classes = useStyles();
+    const { connection } = useChat();
+
     const { message } = props;
     const { timestamp, type, author, content } = message;
 
+    const avatarColor = connection ? connection.metadata[author] : generateAvatarColor();;
+
     return (
-        <ListItem key={timestamp} style={{ color: 'white' }}>
-            <Typography variant={'caption'}>{author}</Typography>
-            <Typography>{content}</Typography>
+        <ListItem key={timestamp} className={clsx(classes.item, classes.white)}>
+            {type === 'system' ? (
+                <Alert variant={'outlined'} severity={'info'} className={classes.alert}>
+                    <Typography>{content}</Typography>
+                </Alert>
+            ) : (
+                <Box
+                    display={'flex'}
+                    flexDirection={'row'}
+                    justifyContent={'flex-start'}
+                    alignItems={'center'}
+                >
+                    <Avatar
+                        className={classes.avatar}
+                        style={{ backgroundColor: avatarColor }}
+                    >
+                        <Typography>{author.slice(0, 1).toUpperCase()}</Typography>
+                    </Avatar>
+                    <Box>
+                        <Typography variant={'body2'} className={classes.author}>
+                            {author}
+                        </Typography>
+                        <Typography>{content}</Typography>
+                    </Box>
+                </Box>
+            )}
         </ListItem>
     );
 };
