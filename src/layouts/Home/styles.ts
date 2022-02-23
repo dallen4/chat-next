62c89779c8745c361ef41cdc41b13ae86a38580c
Nieky Allen <nieky.allen@gmail.com>
2022-02-23 02:30:14 -0600

    update home layout

--- src/layouts/Home/styles.ts
+++ src/layouts/Home/styles.ts
@@ -7,8 +7,18 @@ const styles: any = (theme: Theme) =>
             display: 'flex',
         },
         appBar: {
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
             width: `calc(100% - ${drawerWidth}px)`,
             marginLeft: drawerWidth,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
         },
         toolbar: {
             display: 'flex',
@@ -19,7 +29,7 @@ const styles: any = (theme: Theme) =>
         // necessary for content to be below app bar
         toolbarSpacer: theme.mixins.toolbar,
         content: {
-            width: `calc(100% - ${drawerWidth}px)`,
+            width: `100%`,
             height: '100vh',
             maxHeight: '100vh',
             flexGrow: 1,
@@ -27,6 +37,18 @@ const styles: any = (theme: Theme) =>
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'flex-start',
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: -drawerWidth,
+        },
+        contentShift: {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
         },
         bothButtonText: {
             color: theme.palette.primary.main,
