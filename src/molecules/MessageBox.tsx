45562f312bd2267a1f861756676eb4c9eb4b8a6b
Nieky Allen <nieky.allen@gmail.com>
2022-04-04 17:53:14 -0500

    udpte message box styles for mobile

--- src/molecules/MessageBox.tsx
+++ src/molecules/MessageBox.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
-import { Theme, makeStyles, createStyles, Button, Box } from '@material-ui/core';
+import {
+    Theme,
+    makeStyles,
+    createStyles,
+    Button,
+    Box,
+    useTheme,
+    useMediaQuery,
+    IconButton,
+} from '@material-ui/core';
 import MessageInput from 'atoms/MessageInput';
 import { useMessages } from 'contexts/MessagesContext';
+import { Send } from '@material-ui/icons';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -15,10 +26,21 @@ const useStyles = makeStyles((theme: Theme) =>
             justifyContent: 'space-between',
             backgroundColor: theme.palette.primary.light,
         },
+        mobileMessageBox: {
+            height: '60px',
+        },
         sendButton: {
             height: '79px',
             width: '79px',
             marginLeft: '0.5rem',
+        },
+        mobileSendButton: {
+            height: '44px',
+            width: '44px',
+            marginLeft: '0.5rem',
+            borderRadius: '50%',
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.common.white,
         },
     }),
 );
@@ -26,6 +48,9 @@ const useStyles = makeStyles((theme: Theme) =>
 const MessageBox = ({ disabled }: MessageBoxProps) => {
     const classes = useStyles();
     const [messageInput, setMessageInput] = useState('');
+
+    const theme = useTheme();
+    const mobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     const { sendMessage } = useMessages();
 
@@ -37,23 +62,40 @@ const MessageBox = ({ disabled }: MessageBoxProps) => {
         }
     };
 
+    const btnProps = {
+        disabled: disabled || messageInput.length === 0,
+        onClick: onSend,
+    };
+
     return (
-        <Box className={classes.messageBox}>
+        <Box className={clsx(classes.messageBox, {
+            [classes.mobileMessageBox]: mobile,
+        })}>
             <MessageInput
                 messageInput={messageInput}
                 setMessageInput={setMessageInput}
                 onSubmit={onSend}
                 disabled={disabled}
             />
-            <Button
-                disabled={disabled || messageInput.length === 0}
-                variant={'contained'}
-                color={'primary'}
-                onClick={onSend}
-                className={classes.sendButton}
-            >
-                Send
-            </Button>
+            {mobile ? (
+                <IconButton
+                    color={'primary'}
+                    size={'small'}
+                    className={classes.mobileSendButton}
+                    {...btnProps}
+                >
+                    <Send />
+                </IconButton>
+            ) : (
+                <Button
+                    variant={'contained'}
+                    color={'primary'}
+                    className={classes.sendButton}
+                    {...btnProps}
+                >
+                    Send
+                </Button>
+            )}
         </Box>
     );
 };
